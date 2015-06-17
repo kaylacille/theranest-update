a38aac85a4b6ad18640e4c43622324ef421f4c51
kaylacille <kaylacille@gmail.com>
2015-06-17 11:14:04 -0500

    Initial upload of new site

---
+++ assets/js/custom.js
@@ -0,0 +1,21 @@
+(function($) {
+
+// prettyPhoto
+	jQuery(document).ready(function(){
+		jQuery('a[data-gal]').each(function() {
+			jQuery(this).attr('rel', jQuery(this).data('gal'));
+		});  	
+		jQuery("a[data-rel^='prettyPhoto']").preyPhoto({animationSpeed:'slow',theme:'light_square',slideshow:false,overlay_gallery: false,social_tools:false,deeplinking:false});
+	}); 
+
+		
+})(jQuery);
+
+// When the DOM is ready, run this function
+$(document).ready(function() {
+  //Set the carousel options
+  $('#quote-carousel').carousel({
+    pause: true,
+    interval: 4000,
+  });
+});
